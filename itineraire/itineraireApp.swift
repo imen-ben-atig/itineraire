3955cfcdc20bacf0ebb1a3e61595ffd55700b1d3
imenba <imen.benatig@esprit.tn>
2023-11-03 22:49:50 +0100

    Initial Commit

---
+++ itineraire/itineraireApp.swift
@@ -0,0 +1,17 @@
+//
+//  itineraireApp.swift
+//  itineraire
+//
+//  Created by imba on 3/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct itineraireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
